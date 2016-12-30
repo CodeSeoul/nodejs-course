3f9404db8c0c2e4f98852a273b4617b2f23adb43
Dale Seo <dale.seo@gmail.com>
2016-12-30 18:49:48 +0900

    Update app.js

--- lesson7/challenges/app.js
+++ lesson7/challenges/app.js
@@ -1,2 +1,6 @@
+// Notice the two different files: high_five.js and app.js. 
+// The code as it's written will not work, high_five.js isn't exporting anything.
+// Add the proper exports line to have a successful high five!
+
 var highfive = require('./high_five.js');
 highfive();
