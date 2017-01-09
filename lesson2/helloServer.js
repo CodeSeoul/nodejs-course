f6369ef08813f2e0e15c0a2fb34637060256427c
Dale Seo <dale.seo@gmail.com>
2017-01-09 22:08:02 +0900

    Update helloServer.js

--- lesson2/helloServer.js
+++ lesson2/helloServer.js
@@ -4,6 +4,6 @@ var server = http.createServer(function(req, res) {
   res.end('Hello, World!'); // Send the response body as "Hello, World!"
 });
 
-server.listen(8080);
+server.listen(3000);
 
-console.log('Server running at http://localhost:8080');
+console.log('Server running at http://localhost:3000');
