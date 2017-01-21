1a14b3c0c2e485eba87ecd64769a7bfea2ab013f
khpark <khpark@ge.com>
2017-01-21 13:23:31 +0900

    3rd mission

--- lesson1/challenges/function-arguments.js
+++ lesson1/challenges/function-arguments.js
@@ -10,3 +10,30 @@ Within the 'getFullName' function, return the full name according to the country
 After that, inside the parentheses of console.log(), call the getFullName() function 
 with your first and last name and country name as the arguments.
 */
+
+
+// Named Function
+function getFullName("Dale", "Seo", "KR")` {
+    console.log('Hello,', name, '!');
+    return true;
+}
+
+// Anonymous Function
+var hello = function(name) {
+    console.log('Hello,', name, '!');
+    return true;
+};
+
+// ES6 Arrow Function (a.k.a. lambda)
+var hello = (name) => {
+    console.log('Hello,', name, '!');
+    return true;
+};
+
+var hello = (name) => {
+    console.log('Hello,', name, '!');
+    return true;
+};
+
+var `getFullName("Dale", "Seo", "KR")` should return `Seo Dale`.
+    - ex2: `getFullName("Dale", "Seo", "US")` should return `Dale Seo`.
